b968b6cad49be1d625843d52c28a4ca9220c1f30
vinitkanhasoft <vinit.kanhasoft@gmail.com>
2026-03-10 12:35:58 +0530

    addatra

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 };
 
